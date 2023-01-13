99d9bdb575791e519c56c2badde8bf097b5083ee
Dennis Miczek <platinumtea7@gmail.com>
2023-01-12 23:32:14 -0500

    Boilerplate Win32 code

--- SimX/main.cpp
+++ SimX/main.cpp
@@ -1,9 +1,56 @@
-#include <windows.h>
-#include <d3d11.h>
+#ifndef UNICODE
+#define UNICODE
+#endif
 
-INT WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, INT nCmdShow) {
-	
-	
+#include <windows.h>
+
+LRESULT CALLBACK mainProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) {
+	const wchar_t CLASS_NAME[] = L"PhysicsSim";
+
+	WNDCLASS wc = {};
+	wc.lpfnWndProc = mainProc;
+	wc.hInstance = hInstance;
+	wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wc.lpszClassName = CLASS_NAME;
+
+	RegisterClass(&wc);
+
+	HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"SimX", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+	if (hwnd == NULL) {
+		return 0;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+
+	MSG msg{};
+
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
 	return 0;
 
+}
+
+LRESULT CALLBACK mainProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	switch (uMsg) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+
+		FillRect(hdc, &ps.rcPaint, (HBRUSH)GetStockObject(BLACK_BRUSH));
+		EndPaint(hWnd, &ps);
+	}
+	return 0;
+	}
+
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
